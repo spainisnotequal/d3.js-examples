fafe719469d61201ec934cf6ecd8ced1487cf1d5
spainisnotequal <spainisnotequal@gmail.com>
2020-05-01 19:59:26 +0200

    Add a basic bar chart

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+// create the Express app
+const app = express();
+
+// Express JSON parser
+app.use(express.json());
+
+// Serve static files in the "public" directory
+app.use("/", express.static(path.join(__dirname, "public")));
+
+// configure the port
+const PORT = process.env.PORT || 5000;
+
+// start the server and log that the server is up and running
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
