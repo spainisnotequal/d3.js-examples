9f38689a8d6b319f08671d00e5014268d23cb3da
spainisnotequal <spainisnotequal@gmail.com>
2020-05-03 21:43:27 +0200

    Define the function that draws the bar labels

--- public/javascript/bar-chart-race-2.js
+++ public/javascript/bar-chart-race-2.js
@@ -120,10 +120,79 @@ d3.csv("../data/category-brands.csv", d3.autoType).then((data) => {
         ));
   }
 
+  // labels function: initializes the component, such as by adding a G element, and returns an update function which will be called repeatedly to draw labels
+  function labels(svg) {
+    let label = svg
+      .append("g")
+      .style("font", "bold 12px var(--sans-serif)")
+      .style("font-variant-numeric", "tabular-nums")
+      .attr("text-anchor", "end")
+      .selectAll("text");
+
+    return ([date, data], transition) =>
+      (label = label
+        .data(data.slice(0, n), (d) => d.name)
+        .join(
+          (enter) =>
+            enter
+              .append("text")
+              .attr(
+                "transform",
+                (d) =>
+                  `translate(${x((prev.get(d) || d).value)},${y(
+                    (prev.get(d) || d).rank
+                  )})`
+              )
+              .attr("y", y.bandwidth() / 2)
+              .attr("x", -6)
+              .attr("dy", "-0.25em")
+              .text((d) => d.name)
+              .call((text) =>
+                text
+                  .append("tspan")
+                  .attr("fill-opacity", 0.7)
+                  .attr("font-weight", "normal")
+                  .attr("x", -6)
+                  .attr("dy", "1.15em")
+              ),
+          (update) => update,
+          (exit) =>
+            exit
+              .transition(transition)
+              .remove()
+              .attr(
+                "transform",
+                (d) =>
+                  `translate(${x((next.get(d) || d).value)},${y(
+                    (next.get(d) || d).rank
+                  )})`
+              )
+              .call((g) =>
+                g
+                  .select("tspan")
+                  .tween("text", (d) =>
+                    textTween(d.value, (next.get(d) || d).value)
+                  )
+              )
+        )
+        .call((bar) =>
+          bar
+            .transition(transition)
+            .attr("transform", (d) => `translate(${x(d.value)},${y(d.rank)})`)
+            .call((g) =>
+              g
+                .select("tspan")
+                .tween("text", (d) =>
+                  textTween((prev.get(d) || d).value, d.value)
+                )
+            )
+        ));
+  }
+
   // /* ------------------- */
   // /* TEXT TWEEN FUNCTION */
   // /* ------------------- */
-  // // D3 doesn’t transition text by default, so we need a function to do it
+  // textTween function: D3 doesn’t transition text by default, so we need a function to do it
   // function textTween(a, b) {
   //   const i = d3.interpolateNumber(a, b);
   //   return function (t) {
