b370ad5531e4375a9b5c2a53d5f59e8fb74bf39d
spainisnotequal <spainisnotequal@gmail.com>
2020-05-04 16:19:35 +0200

    Copy the main code from the example (source: observablehq.com) (currently, the code doesn't work)

--- public/javascript/bar-chart-race-2.js
+++ public/javascript/bar-chart-race-2.js
@@ -261,4 +261,32 @@ d3.csv("../data/category-brands.csv", d3.autoType).then((data) => {
   /* ------------- */
   /* START DRAWING */
   /* ------------- */
+  (async () => {
+    const svg = d3.create("svg").attr("viewBox", [0, 0, width, height]);
+
+    const updateBars = bars(svg);
+    const updateAxis = axis(svg);
+    const updateLabels = labels(svg);
+    const updateTicker = ticker(svg);
+
+    svg.node();
+
+    for (const keyframe of keyframes) {
+      const transition = svg
+        .transition()
+        .duration(duration)
+        .ease(d3.easeLinear);
+
+      // Extract the top bar’s value.
+      x.domain([0, keyframe[1][0].value]);
+
+      updateAxis(keyframe, transition);
+      updateBars(keyframe, transition);
+      updateLabels(keyframe, transition);
+      updateTicker(keyframe, transition);
+
+      invalidation.then(() => svg.interrupt());
+      await transition.end();
+    }
+  })();
 });
